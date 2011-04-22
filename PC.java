483011faf08b1f9bbdf31e42b559f0e75f990e20
cpetrie <cpetrie@lappy486>
2011-04-22 16:50:37 -0400

    Copied TestSerial.java in, editing.

---
+++ PC.java
@@ -0,0 +1,90 @@
+/**
+ * Java-side application for recording LED state information
+ * 
+ *
+ * @author Eric Willisson <epw@wpi.edu>
+ * @date 20110422
+ */
+
+import java.io.IOException;
+
+import net.tinyos.message.*;
+import net.tinyos.packet.*;
+import net.tinyos.util.*;
+
+public class TestSerial implements MessageListener {
+
+  private MoteIF moteIF;
+  
+  public TestSerial(MoteIF moteIF) {
+    this.moteIF = moteIF;
+    this.moteIF.registerListener(new TestSerialMsg(), this);
+  }
+
+  public void sendPackets() {
+    int counter = 0;
+    TestSerialMsg payload = new TestSerialMsg();
+    
+    try {
+      while (true) {
+	System.out.println("Sending packet " + counter);
+	payload.set_counter(counter);
+	moteIF.send(0, payload);
+	counter++;
+	try {Thread.sleep(1000);}
+	catch (InterruptedException exception) {}
+      }
+    }
+    catch (IOException exception) {
+      System.err.println("Exception thrown when sending packets. Exiting.");
+      System.err.println(exception);
+    }
+  }
+
+  public void messageReceived(int to, Message message) {
+    TestSerialMsg msg = (TestSerialMsg)message;
+    System.out.println("Received packet sequence number " + msg.get_counter());
+  }
+  
+  private static void usage() {
+    System.err.println("usage: TestSerial [-comm <source>]");
+  }
+  
+  public static void main(String[] args) throws Exception {
+    String source = null;
+    if (args.length == 2) {
+      if (!args[0].equals("-comm")) {
+	usage();
+	System.exit(1);
+      }
+      source = args[1];
+    }
+    else if (args.length != 0) {
+      usage();
+      System.exit(1);
+    }
+    
+    PhoenixSource phoenix;
+    
+    if (source == null) {
+      phoenix = BuildSource.makePhoenix(PrintStreamMessenger.err);
+    }
+    else {
+      phoenix = BuildSource.makePhoenix(source, PrintStreamMessenger.err);
+    }
+   
+    System.out.println ("got phoenix");
+   
+    MoteIF mif = new MoteIF(phoenix);
+
+	System.out.println ("got new mote");
+	
+    TestSerial serial = new TestSerial(mif);
+
+    System.out.println ("got new TestSerial");
+
+    serial.sendPackets();
+  }
+
+
+}
